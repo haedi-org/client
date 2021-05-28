f78ee5faa4e5015a2d1f0964c0d8e42f30e354a4
Louis Machin <ldhmachin@gmail.com>
2021-05-28 22:41:42 +0100

    Added context isolation

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ function createWindow () {
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true,
-            enableRemoteModule: true
+            enableRemoteModule: true,
+            contextIsolation: false,
         }
     })
     
