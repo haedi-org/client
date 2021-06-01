f6c97fff648923f5a7806813637baaaf270fa798
Louis Machin <ldhmachin@gmail.com>
2021-06-01 16:58:43 +0100

    Amending menu.html

--- menu.js
+++ menu.js
@@ -5,3 +5,10 @@ edifactBtn.onclick = onEdifactBtnClick;
 function onEdifactBtnClick() {
     document.location.href = './inspect.html';
 }
+
+const customTitlebar = require('custom-electron-titlebar');
+
+new customTitlebar.Titlebar({
+    backgroundColor: customTitlebar.Color.fromHex('#FFFFFF'),
+    menu: null
+})
